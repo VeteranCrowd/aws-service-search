00e1348b9228edbb57016235b101de14569c1faf
Jason Williscroft <jason.williscroft@veterancrowd.com>
2023-05-13 17:32:22 +0800

    added de/rehydration

---
+++ test/entityConfig.js
@@ -0,0 +1,54 @@
+import { sn2e, sn2u } from '@karmaniverous/tagged-templates';
+
+export default {
+  entities: {
+    group: {
+      indexes: {
+        created: ['entityPK', 'entitySK', 'created'],
+        displayNameCanonical: ['entityPK', 'entitySK', 'displayNameCanonical'],
+        updated: ['entityPK', 'entitySK', 'updated'],
+      },
+      keys: {
+        // Entity PK.
+        entityPK: {
+          encode: ({ entityToken = 'group', shardId }) =>
+            `${entityToken}!${sn2e`${shardId}`}`,
+          decode: (value) =>
+            value.match(/^(?<entityToken>.*)!(?<shardId>.*)$/)?.groups,
+        },
+
+        // Entity SK.
+        entitySK: {
+          encode: ({ groupId }) => sn2u`groupId#${groupId}`,
+          decode: (value) => value.match(/^groupId#(?<groupId>.*)$/)?.groups,
+        },
+
+        // Group created SK.
+        created: {
+          encode: ({ created }) => Number(created),
+          decode: (value) => ({ created: value.toString() }),
+          retain: true,
+        },
+
+        // Group displayName SK.
+        displayNameCanonical: {
+          encode: ({ displayNameCanonical }) => displayNameCanonical,
+          decode: (value) => ({ displayNameCanonical: value }),
+          retain: true,
+        },
+
+        // Group updated SK.
+        updated: {
+          encode: ({ updated }) => Number(updated),
+          decode: (value) => ({ updated: value.toString() }),
+          retain: true,
+        },
+      },
+      sharding: {
+        entityKey: ({ groupId }) => groupId,
+        timestamp: ({ created }) => created,
+      },
+    },
+  },
+  shardKeyToken: 'shardId',
+};
